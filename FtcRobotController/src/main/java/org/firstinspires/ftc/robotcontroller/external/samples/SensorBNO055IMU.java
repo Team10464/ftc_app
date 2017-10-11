dc32c39edb3067be069a45fb1c43dab482abe181
Emily Kiehl <emilydkiehl@gmail.com>
2017-10-11 19:21:24 -0400

    Vuforia and oriented protobot system

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorBNO055IMU.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorBNO055IMU.java
@@ -90,6 +90,7 @@ public class SensorBNO055IMU extends LinearOpMode
         // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
         // and named "imu".
         imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.getPosition();
         imu.initialize(parameters);
 
         // Set up our telemetry dashboard
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumProtoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumProtoBot.java
@@ -1,34 +1,50 @@
 package org.firstinspires.ftc.teamcode;
 
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.GyroSensor;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorBNO055IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 /**
  * Created by jonathonmangan on 10/1/17.
  */
+@TeleOp(name="Mechanum Protobot Tank", group="Protobot")
 
-public class MechanumProtoBot extends OpMode {
+public class MechanumProtoBot extends OpMode
+{
+
+    // State used for updating telemetry
+    Orientation angles;
+    Acceleration gravity;
     private DcMotor motorFrontRight;
     private DcMotor motorFrontLeft;
     private DcMotor motorBackRight;
     private DcMotor motorBackLeft;
-    private GyroSensor gyro;
+    private BNO055IMU imu;
 
-    public void init(){
+    public void init()
+    {
         motorFrontRight = hardwareMap.dcMotor.get("frontRight");
         motorFrontLeft= hardwareMap.dcMotor.get("frontLeft");
         motorBackRight = hardwareMap.dcMotor.get("backRight");
         motorBackLeft = hardwareMap.dcMotor.get("backLeft");
 
-        gyro = hardwareMap.gyroSensor.get("gyro");
-        gyro.calibrate();
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.getCalibrationStatus();
+
+
     }
-    public void loop(){
-        int heading = gyro.getHeading();
+    public void loop()
+    {
+        int reading = imu.readCalibrationData();
 
         double power = ((Math.abs(gamepad1.left_stick_y)+Math.abs(gamepad1.left_stick_x))/2);
-        double head = (Math.PI * heading)/180;
+        double head = (Math.PI * reading)/180;
         double headSticks = 2;
 
         double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);

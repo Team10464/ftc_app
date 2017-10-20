346ec3c8e0aa7b8e5f9b2c5d26d91be93b39d116
joannareese <jdreese2016@gmail.com>
2017-10-20 13:23:42 -0400

    gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.*;
+
 
 /**
  * Created by joannareese on 10/20/17.

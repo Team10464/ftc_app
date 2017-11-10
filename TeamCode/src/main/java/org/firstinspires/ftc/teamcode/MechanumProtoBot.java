6486850fb7d42dd3b69946bdfe2647c659047792
Nathanous Trawets <natedawg1121@gmail.com>
2017-11-10 11:58:13 -0500

    nathan did something to the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumProtoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumProtoBot.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -8,13 +7,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-/**
- * Created by jonathonmangan on 10/1/17.
- */
 @TeleOp(name="Mechanum Protobot Tank", group="Protobot")
 
-public class MechanumProtoBot extends OpMode
-{
+public class MechanumProtoBot extends OpMode    {
 
     // State used for updating telemetry
     Orientation angles;
@@ -23,23 +18,14 @@ public class MechanumProtoBot extends OpMode
     private DcMotor motorFrontLeft;
     private DcMotor motorBackLeft;
     private DcMotor motorBackRight;
-    private DcMotor conveyorHorz;
-    private DcMotor conveyorVert;
-    private BNO055IMU imu;
 
-    public void init()
-    {
+    public void init()  {
         motorFrontRight = hardwareMap.dcMotor.get("frontRight");
         motorFrontLeft= hardwareMap.dcMotor.get("frontLeft");
         motorBackRight = hardwareMap.dcMotor.get("backLeft");
-        motorBackLeft = hardwareMap.dcMotor.get("backRight");
+        motorBackLeft = hardwareMap.dcMotor.get("backRight");   }
 
-
-    }
-    public void loop()
-    {
-
-
+    public void loop()  {
         double r = Math.hypot(gamepad1.right_stick_x, gamepad1.left_stick_y);
         double robotAngle = Math.atan2(gamepad1.right_stick_x, gamepad1.left_stick_y) - Math.PI / 4;
         double rightX = gamepad1.left_stick_x;
@@ -51,10 +37,6 @@ public class MechanumProtoBot extends OpMode
         motorFrontRight.setPower(v1);
         motorFrontLeft.setPower(v2);
         motorBackRight.setPower(v3);
-        motorBackLeft.setPower(v4);
-
-    }
-
-}
+        motorBackLeft.setPower(v4); }   }
 
 
